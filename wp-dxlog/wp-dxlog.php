12fb2f3e40c31c8f10f2404dc4a82e6cbfafd88b
Mike Clarke <clarkema@gmail.com>
2008-09-16 17:27:23 +0100

    Searching code now works for a single event.

--- wp-dxlog/wp-dxlog.php
+++ wp-dxlog/wp-dxlog.php
@@ -25,12 +25,97 @@ add_shortcode( 'dxlog-stats', 'dxlog_shortcode_stats' );
 
 function dxlog_shortcode_search ( $attributes )
 {
-    $form = '<form action="' . get_permalink() . '" method="post">';
-    $form .= '<input type="text">';
-    $form .= '</form>';
-    return $form;
+    $form = "";
+    $search_term = $_POST['wp-dxlog-callsign-search'];
 
+    if ( $search_term ) {
+        $form .= "<p>$search_term has worked the following bands and modes:</p>";
+        $form .= dxlog_qso_table( dxlog_search( $search_term ) );
+
+        $form .= "Search again?";
+    }
+
+    $form .= '<form action="' . get_permalink() . '" method="post">';
+    $form .= '<input name="wp-dxlog-callsign-search" type="text">';
+    $form .= '<input value="Search" type="submit">';
+    $form .= '</form>';
+
+    return $form;
 }
 add_shortcode( 'dxlog-search', 'dxlog_shortcode_search' );
+
+function dxlog_search ( $callsign )
+{
+    // FIXME: namespacing issues
+    require_once( 'wp-content/plugins/wp-dxlog/dbinc.php' );
+
+    $toreturn = array();
+
+    $dbh = @mysql_connect( $hostName, $username, $password );
+
+    if ( ! is_resource( $dbh ) ) {
+        // Death
+    }
+    else {
+        @mysql_select_db( $databaseName, $dbh );
+
+        $query = sprintf( "SELECT * FROM qsos WHERE UPPER(callsign) = UPPER('%s')",
+                        $callsign );
+
+        $result = @mysql_query( $query, $dbh );
+
+        while ( $row = @mysql_fetch_assoc( $result ) ) {
+            $toreturn[$row['band']][$row['op_mode']] = 1;
+        }
+
+        mysql_free_result( $result );
+    }
+
+    return $toreturn;
+}
+
+function dxlog_qso_table ( $results )
+{
+    $modes = array( 'SSB', 'Data', 'CW' );
+    sort($modes);
+
+    $bandmodes['160'] = array( 'SSB' => 1, 'Data' => 1, 'CW' => 1 );
+    $bandmodes['80']  = array( 'SSB' => 1, 'Data' => 1, 'CW' => 1 );
+    $bandmodes['40']  = array( 'SSB' => 1, 'Data' => 1, 'CW' => 1 );
+    $bandmodes['20']  = array( 'SSB' => 1, 'Data' => 1, 'CW' => 1 );
+    $bandmodes['17']  = array( 'SSB' => 1, 'Data' => 1, 'CW' => 1 );
+    $bandmodes['15']  = array( 'SSB' => 1, 'Data' => 1, 'CW' => 1 );
+    $bandmodes['10']  = array( 'SSB' => 1, 'Data' => 1, 'CW' => 1 );
+    $bandmodes['6']   = array( 'SSB' => 1, 'Data' => 1, 'CW' => 1 );
+    $bandmodes['4']   = array( 'SSB' => 1, 'Data' => 1, 'CW' => 1 );
+    $bandmodes['2']   = array( 'SSB' => 1, 'Data' => 1, 'CW' => 1 );
+
+    $toreturn = "<table class='wp-dxlog-search-results'>";
+
+    $toreturn .= "<tr><th>Band</th>";
+    foreach ( $modes as $mode ) {
+        $toreturn .= "<th>$mode</th>";
+    }
+    $toreturn .= "</tr>";
+
+    foreach ( $bandmodes as $band => $modes_for_band ) {
+        $toreturn .= "<tr><td>$band</td>";
+
+        foreach ( $modes as $mode ) {
+            if ( $results[$band][$mode] ) {
+                $toreturn .= "<td>X</td>";
+            }
+            else {
+                $toreturn .= "<td>&nbsp;</td>";
+            }
+        }
+
+        $toreturn .= "</tr>";
+    }
+
+    $toreturn .= "</table>";
+
+    return $toreturn;
+}
 
 ?>
